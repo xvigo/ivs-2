b1aa95111196bedb413662577b312d486d302add
Pavel Marek <xmarek75@vutbr.cz>
2021-03-05 20:41:21 +0100

    add basic functions

--- ivs.py
+++ ivs.py
@@ -1,2 +1,18 @@
 print('git is life')
 print('stepa je mekota')
+
+# Function to add two numbers  
+def add(num1, num2): 
+    return num1 + num2 
+  
+# Function to subtract two numbers  
+def subtract(num1, num2): 
+    return num1 - num2 
+  
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+    return num1 * num2 
+  
+# Function to divide two numbers 
+def divide(num1, num2): 
+    return num1 / num2 
