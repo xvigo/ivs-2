13a622ed0ae763ff1f42cc3af4bb3a13f9b5ca8e
Vilém Gottwald <xgottw07@vutbr.cz>
2021-03-05 21:09:19 +0100

    renamed variables and fuctions to be Mathematical

--- ivs.py
+++ ivs.py
@@ -1,15 +1,15 @@
 # Function to add two numbers  
-def add(num1, num2): 
-    return num1 + num2 
+def addition(addend1, addend2): 
+    return addend1 + addend2 
   
 # Function to subtract two numbers  
-def subtract(num1, num2): 
-    return num1 - num2 
+def subtraction(minuend, subtrahend): 
+    return minuend - subtrahend 
   
 # Function to multiply two numbers 
-def multiply(num1, num2): 
-    return num1 * num2 
+def multiplication(factor1, factor2): 
+    return factor1 * factor2 
   
 # Function to divide two numbers 
-def divide(num1, num2): 
-    return num1 / num2 
+def division(dividend, divisor): 
+    return dividend / divisor
