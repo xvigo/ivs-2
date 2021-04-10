a6b94cff9ade0666f2f399961d3081e68196cce3
Vilém Gottwald <xgottw07@vutbr.cz>
2021-04-10 11:33:12 +0200

    implemented kezboard input into calculator

--- src/gui.py
+++ src/gui.py
@@ -43,63 +43,79 @@ label.image = photo
 label.grid(row = 0, column = 0, rowspan = 2)
 
 
-#Entry screen to display numbers and operations
+#Entry screen to display history
 t = tk.Entry(root,justify = tk.LEFT, width = 25, bg = "#303030", bd = 0, highlightbackground = hbgc, font = ('Helvetica', 20))
-# input_text = StringVar()
-
-# input_frame = Frame(root, width = 312, height = 50, bd = 0, highlightbackground = "black", highlightcolor = "black", highlightthickness = 1)
-
-# input_frame.pack(side = TOP)
-
-# t = Entry(input_frame, font = ('arial', 18, 'bold'), textvariable = input_text, width = 50, bg = "#eee", bd = 0, justify = RIGHT)
 
 
+def validCheck(action, char):
+    if action == '0':
+        return True
+    global ans_displayed
+    allowed = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "/", "*", "-", "+", "^", "%", "!", "√"]
+    nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
 
-t2 = tk.Entry(root,justify = tk.RIGHT, width = 15, bg = "#404040", bd = 0, highlightbackground = hbgc, font = ('Helvetica', 35))
 
+    for i in char:
+        if i not in allowed:
+            return False
+
+    if i in nums and ans_displayed:
+        t2.delete(0, tk.END)
+        t2.insert(tk.END, char)
+        t2.after_idle(lambda: t2.configure(validate="all"))
+    ans_displayed = False
+    return True
+
+
+#Entry screen to display current input
+VC = root.register(validCheck)
+t2 = tk.Entry(root, justify = tk.RIGHT, validate='key', validatecommand=(VC, '%d', '%S'), width = 15, bg = "#404040", bd = 0, highlightbackground = hbgc, font = ('Helvetica', 35))
+t2.focus_set()
 t2.grid(row = 1, column = 1, columnspan = 5, sticky = tk.N)
 t.grid(row = 0, column = 1, columnspan = 5, sticky = tk.S)
 
-ans = 1
+ans_displayed = False
+glob_result = ""
 #function to add buttonnumber to the entry
 
 def b_num(new):
-    global ans
-    if ans == 0:
+    global ans_displayed
+    if ans_displayed == True:
        b_clear_empty()
     current = t2.get()
     t2.delete(0, tk.END)
     formula = str(current) + str(new)
     t2.insert(0, formula)
-    ans = 1
+    ans_displayed = False
     return
 
 def b_operator(new):
-    global ans
+    global ans_displayed
     current = t2.get()
     t2.delete(0, tk.END)
     formula = str(current) + str(new)
     t2.insert(0, formula)
-    ans = 1
+    ans_displayed = False
     return
 
-def b_equal():
-    global ans
+def b_equal(event=None):
+    global ans_displayed
+    global glob_result
     expr = t2.get()
     if len(expr) == 0:
         return
-    t.delete(0, tk.END)
 
     result = pe.solve_expr(expr)
-
+    glob_result = result
     t.delete(0, tk.END)
     t.insert(0, str(expr)+str(" = "))
     t2.delete(0, tk.END)
     t2.insert(0, str(result))
-    ans = 0
+    ans_displayed = True
     return
 
-
+root.bind('<Return>', b_equal)
+root.bind('<KP_Enter>', b_equal)
 def b_delete():
     index = len(t2.get()) - 1
     t2.delete(index, tk.END)
@@ -109,12 +125,14 @@ def b_clear_empty():
     t.delete(0, tk.END)
     return
 
-def b_abs():
+#UNUSED!
+"""
+def b_abs(event):
     val = t2.get()
     t2.delete(0, tk.END)
     t2.insert(0, str(abs(pe.solve_expr(val))))
     return
-
+"""
 #creating number
 button_1 = tk.Button(root, text = "1", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_num(1))
 button_2 = tk.Button(root, text = "2", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_num(2))
@@ -136,18 +154,18 @@ button_divide = tk.Button(root, text= "/", font = myFont, activebackground = act
 button_equal = tk.Button(root, text = "=", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_equal())
 button_power = tk.Button(root, text = "^", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_operator("^"))
 button_root = tk.Button(root, text = "√", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_num("√"))
-button_abs = tk.Button(root, text = "ABS", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_abs())
+button_ans = tk.Button(root, text = "ANS", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_num(glob_result))
 button_del = tk.Button(root, text = "DEL", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_delete())
 button_ac = tk.Button(root, text = "AC", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_clear_empty())
-button_ce = tk.Button(root, text = "MOD", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_operator("%"))
+button_mod = tk.Button(root, text = "MOD", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_operator("%"))
 button_fact = tk.Button(root, text = "!", font = myFont, activebackground = active_color, bd = 0, highlightbackground = hbgc, bg = button_color, padx = padx_size, pady = pady_size, height = 2, width = 5, command = lambda: b_operator("!"))
 
 
 
 #griding buttons to the root window
-button_abs.grid(row = 2, column = 0)
-button_ac.grid(row = 2, column = 1)
-button_ce.grid(row = 2, column = 2)
+button_ans.grid(row = 2, column = 0)
+button_mod.grid(row = 2, column = 1)
+button_ac.grid(row = 2, column = 2)
 button_del.grid(row = 2, column = 3)
 
 button_power.grid(row = 3, column = 0)
